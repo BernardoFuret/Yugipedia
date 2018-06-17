d77854e4c214fbcb0788fde13b02318689216e45
BernardoFuret <bernardo.furet@tecnico.ulisboa.pt>
2018-06-18 00:02:15 +0100

    Add Gadgets foler with one Gadget.

---
+++ Gadgets/README.md
@@ -0,0 +1,6 @@
+#About
+Yugipedia has the extension [Gadgets][1] enabled.
+This folder is for Gadgets only (the file names are prefixed by "Gadget-", on wiki).
+
+
+[1]: https://www.mediawiki.org/wiki/Extension:Gadgets
---
+++ Gadgets/ShowUserGroupsTags/ShowUserGroupsTags.js
@@ -0,0 +1,63 @@
+/**
+ * Displays the user groups on the main user and main user talk pages.
+ * @author Becasita
+ * @contact [[User talk:Becasita]]
+ */
+( function _showUserGroupsTags( $, mw, console ) {
+	"use strict";
+
+	var LAST_LOG = '~~~~~';
+
+	// Get config vars:
+	var config = mw.config.get( [
+		'wgNamespaceNumber',
+		'wgPageName'
+	] );
+
+	// Check if on the user or user talk namespace:
+	if ( config.wgNamespaceNumber !== 2 && config.wgNamespaceNumber !== 3 ) {
+		// It's not:
+		return;
+	}
+
+	// Make an api call for user groups:
+	new mw.Api()
+		.get( {
+			action: 'query',
+			list: 'users',
+			ususers: config.wgPageName.replace( /^user((_| )talk)?:/im, '' ),
+			usprop: 'groups'
+		} )
+		.done( function( data ) {
+			// Valid groups to display:
+			var validGroups = {
+				bureaucrat: 'BCRAT',
+				sysop: 'ADMIN'
+			};
+
+			try {
+				// Iterate over all groups:
+				data.query.users[ 0 ].groups.forEach( function( group, index ) {
+					// Check if it's worth showing a tag for this group:
+					var validGroup = validGroups[ group ];
+
+					if ( validGroup ) {
+						$( '#firstHeading' ).append(
+							$( '<span>', {
+								id: 'user-group-tag-' + validGroup,
+								class: 'user-group-tag',
+								text: validGroup
+							} )
+						);
+					}
+				} );
+			} catch ( e ) {
+				console.warn( 'Caught error @_showUserGroupsTags():', e );
+			}
+		} )
+		.fail( console.error );
+
+		// Log:
+		console.log( 'ShowUserGroupsTags last updated at', LAST_LOG );
+
+} )( window.jQuery, window.mediaWiki, window.console );
