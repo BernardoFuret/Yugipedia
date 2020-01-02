067187e1bd1be3cb88fc37e8d575933fb8d8d9b8
BernardoFuret <bernardo.furet@tecnico.ulisboa.pt>
2020-01-02 15:18:08 +0000

    Add two video games static modules (release and name).

---
+++ Modules/Data/namespaces/videoGames/static/name/index.lua
@@ -0,0 +1,61 @@
+-- <pre>
+local thisData = mw.loadData( 'Module:Data/namespaces/videoGames/static/name/data' )
+
+local function normalize( v )
+	if type( v ) ~= 'string' then
+		return nil
+	end
+	
+	local normalizedV = table.concat( { v, ' ' } )
+		:lower()
+		-- Replace roman numerals:
+		:gsub(   ' viii[!: ]', '8' )
+		:gsub(    ' vii[!: ]', '7' )
+		:gsub(     ' vi[!: ]', '6' )
+		:gsub(      ' v[!: ]', '5' )
+		:gsub(     ' iv[!: ]', '4' )
+		:gsub(    ' iii[!: ]', '3' )
+		:gsub(     ' ii[!: ]', '2' )
+		:gsub(      ' i[!: ]', '1' )
+		-- Remove a bunch of commonly used characters:
+		:gsub( "[%s%-_'/:! ]",  '' )
+		-- Remove series names:
+		:gsub(       'yugioh',  '' )
+		:gsub(          '5ds',  '' )
+		:gsub(        'zexal',  '' )
+		:gsub(         'arcv',  '' )
+		:gsub(       'vrains',  '' )
+		-- Remove some redundant words:
+		:gsub(          'the',  '' )
+		:gsub(      'gameboy',  '' )
+		-- Normalize some titles:
+		:gsub(      'expert', 'ex' )
+		:gsub( 'worldchampionshiptournament', 'worldchampionship' )
+
+	-- Remove "gx", if it's large enough (preserve "gx01", "gx3", "ygoo", "ygo" ):
+	if normalizedV:len() > 4 then
+		normalizedV = normalizedV
+			:gsub(  'gx', '' )
+			:gsub( 'ygo', '' )
+		
+		-- Remove "duelmonsters", if it's large enough (preserve cases like /duelmonsters\d/):
+		if normalizedV:len() > 13 then
+			normalizedV = normalizedV:gsub( 'duelmonsters', '' )
+			-- Remove "worldchampionship", if it's large enough (preserve cases like /worldchampionship20\d\d/):
+			if normalizedV:len() > 21 then
+				normalizedV = normalizedV:gsub( 'worldchampionship', '' )
+			end
+		end
+	end
+
+	return normalizedV
+end
+
+return function( v )
+	return thisData.main[
+		thisData.normalize[
+			normalize( v )
+		]
+	]
+end
+-- </pre>
