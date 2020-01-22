61cefcfa3180ff3b9d7e90af6d841b03902bb146
BernardoFuret <bernardo.furet@tecnico.ulisboa.pt>
2020-01-22 00:08:50 +0000

    Remove backwards compatibility code.

--- Modules/Data/index.lua
+++ Modules/Data/index.lua
@@ -1,17 +1,3 @@
 -- <pre>
-local DATA = require( 'Module:Data/loader' )()
-
-return setmetatable( {
-	getAnimeRelease = DATA.anime.getRelease,
-	getAnimeSeries = DATA.anime.getSeries,
-
-	getMangaRelease = DATA.manga.getRelease,
-	getMangaSeries = DATA.manga.getSeries,
-
-	getVideoGameRelease = DATA.videoGames.getRelease,
-	getVideoGame = DATA.videoGames.getName,
-}, {
-	__index = DATA,
-	__call  = getmetatable( DATA ).__call,
-} )
+return require( 'Module:Data/loader' )()
 -- </pre>
