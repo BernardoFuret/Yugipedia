cc68d51c33f2787ee4e48ac1a5e3cce0e8c348c6
BernardoFuret <bernardo.furet@tecnico.ulisboa.pt>
2020-02-05 00:40:45 +0000

    Tolerate and handle errors from api requests.

--- Gadgets/AjaxSetListTabs/AjaxSetListTabs.js
+++ Gadgets/AjaxSetListTabs/AjaxSetListTabs.js
@@ -13,37 +13,110 @@
 
 	var api;
 
-	function loadList( page ) {
+	function getListData( setListPage ) {
 		return api.get( {
 			action: 'parse',
 			title: pagename,
-			text: [ '{{:', '}}' ].join( page ),
+			text: [ '{{:', '}}' ].join( setListPage )
 		} );
 	}
 
 	function parseData( data ) {
-		return $.parseHTML( data.parse.text[ '*' ] );
+		return $( $.parseHTML( data.parse.text[ '*' ] ) ).html();
 	}
 
-	function displayList( $el, resultHtml ) {
-		var setList = $( resultHtml ).html();
-
-		$el.html( setList );
+	function SetListLoader( $container ) {
+		this.$container = $container;
+		this.defaultContent = $container.html();
+		this.setListPage = $container.data( 'page' );
+		this.remainingTimeoutTries = 2;
 	}
 
-	function logException( e1, e2, e3 ) {
-		console.warn( '[Gadget]', '[AjaxSetListTabs]', 'Error loading list -', e1, e2, e3 );
-	}
+	SetListLoader.prototype.makeError = function() {
+		var self = this;
+
+		var $errorContainer = $( '<div>', {
+			'class': 'set-list-ajax-tab__error'
+		} );
+
+		var $pageLink = $( '<a>', {
+			href: '/wiki/'.concat( this.setListPage ),
+			text: this.setListPage
+		} );
+
+		var $tryAgainLink = $( '<a>', {
+			href: '#',
+			text: 'try again',
+			click: function( e ) {
+				e.preventDefault();
+
+				self.$container.html( self.defaultContent );
+
+				return self.load();
+			}
+		} );
+
+		var $refreshLink = $( '<a>', {
+			href: '#',
+			text: 'refresh this page',
+			click: function( e ) {
+				e.preventDefault();
+
+				window.location.reload();
+			}
+		} );
+
+		return $errorContainer
+			.append( 'Could not load ' )
+			.append( $pageLink )
+			.append( '. Please ' )
+			.append( $tryAgainLink )
+			.append( ' or ' )
+			.append( $refreshLink )
+			.append( '.' )
+		;
+	};
+
+	SetListLoader.prototype.getHtml = function() {
+		var self = this;
+
+		return getListData( this.setListPage )
+			.then( parseData )
+			[ 'catch' ]( function( err1, err2 ) {
+				console.warn(
+					'[Gadget]', '[AjaxSetListTabs]',
+					'Error loading', self.setListPage,
+					'-', err1, err2
+				);
+
+				if ( err2.textStatus === 'timeout' && self.remainingTimeoutTries-- ) {
+					return new Promise( function( resolve ) {
+						window.setTimeout( resolve, Math.random() * 1000 );
+					} )
+						.then( self.getHtml.bind( self ) )
+					;
+				}
+
+				return self.makeError();
+			} )
+		;
+	};
+
+	SetListLoader.prototype.display = function( html ) {
+		this.$container.html( html );
+	};
+
+	SetListLoader.prototype.load = function() {
+		this.getHtml()
+			.then( this.display.bind( this ) )
+		;
+	};
 
 	function loadLists() {
 		$( '.set-list-ajax-tab' ).each( function( i, el ) {
 			var $el = $( el );
 
-			loadList( $el.data( 'page' ) )
-				.then( parseData )
-				.then( displayList.bind( this, $el ) )
-				.catch( logException )
-			;
+			new SetListLoader( $el ).load();
 		} );
 	}
 
