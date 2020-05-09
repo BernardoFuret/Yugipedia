28f2c6bd00fdd86e6181246b2ecc16edba1492cd
BernardoFuret <bernardo.furet@tecnico.ulisboa.pt>
2020-05-09 20:19:23 +0100

    Card artworks are now being tagged with `artwork`.

--- Modules/Data/namespaces/videoGames/static/release/data.lua
+++ Modules/Data/namespaces/videoGames/static/release/data.lua
@@ -2,11 +2,13 @@
 local normalize = {
 	['nc'] = 'nc', ['noncard'] = 'nc',
 	['ca'] = 'ca', ['cardart'] = 'ca', ['art'] = 'ca',
+	['artwork'] = 'artwork',
 }
 
 local main = {
-	['nc'] = { abbr = 'NC', full = 'Non-card' },
-	['ca'] = { abbr = 'CA', full = 'Card art' },
+	['nc']      = { abbr = 'NC',      full = 'Non-card'     },
+	['ca']      = { abbr = 'CA',      full = 'Card artwork' },
+	['artwork'] = { abbr = 'artwork', full = 'Card artwork' },
 }
 
 return {
