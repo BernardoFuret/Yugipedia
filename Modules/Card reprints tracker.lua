73a686e5c4bcde9c06bd117edcd5405618fb5d79
BernardoFuret <bernardo.furet@hotmail.com>
2022-11-13 22:45:22 +0000

    For cards reprint tracker: Slightly optimize (no need to iterate until the end of the collection in some cases). Ignore AE and FC for now.

--- Modules/Card reprints tracker.lua
+++ Modules/Card reprints tracker.lua
@@ -4,49 +4,59 @@
 @description 
 ]=]
 
--- TODO: track count by region. use region object as table index key-
-
 local DATA = require( 'Module:Data' )
 local UTIL = require( 'Module:Util' )
 
 local Reporter = require( 'Module:Reporter' )
 local IndexedCounter = require( 'Module:IndexedCounter' )
-local StringBuffer = require( 'Module:StringBuffer' )
 
 local DATA_REGIONS = DATA( 'region' )
 local DATA_LANGUAGES = DATA( 'language' )
 
-local function computeCategories( arguments )
-	-- local byRegion = IndexedCounter()
+local FRENCH_CANADIAN_REGION = DATA.getRegion( 'fc' )
+local ASIAN_ENGLISH_REGION = DATA.getRegion( 'ae' )
 
+local REGIONS_TO_IGNORE = {
+	[ FRENCH_CANADIAN_REGION.index ] = true,
+	[ ASIAN_ENGLISH_REGION.index ] = true,
+}
+
+local function computeCategories( arguments )
 	local byLanguage = IndexedCounter()
 
 	for _, region in pairs( DATA_REGIONS ) do
-		local setsForRegionParameterName = region.index:lower() .. '_sets'
+		local languageFromRegion = DATA.getLanguage( region.index )
 
-		local setsForRegionArgument = arguments[ setsForRegionParameterName ]
+		local notAnIgnoredRegion = not REGIONS_TO_IGNORE[ region.index ]
 
-		local hasSetsForRegion = UTIL.trim( setsForRegionArgument )
+		if byLanguage:get( languageFromRegion ) <= 1 and notAnIgnoredRegion then
+			-- If not already more than 1, then count.
 
-		if hasSetsForRegion then
-			local linesCountForRegion = 0
+			local setsForRegionParameterName = region.index:lower() .. '_sets'
 
-			for line in mw.text.gsplit( setsForRegionArgument, '\n' ) do
-				local isNonEmptyLine = UTIL.trim( line )
+			local setsForRegionArgument = arguments[ setsForRegionParameterName ]
 
-				if isNonEmptyLine then
-					linesCountForRegion = linesCountForRegion + 1
+			local hasSetsForRegion = UTIL.trim( setsForRegionArgument )
+
+			if hasSetsForRegion then
+				local linesCountForRegion = 0
+
+				for line in mw.text.gsplit( setsForRegionArgument, '\n' ) do
+					local isNonEmptyLine = UTIL.trim( line )
+
+					if isNonEmptyLine then
+						linesCountForRegion = linesCountForRegion + 1
+					end
+
+					if linesCountForRegion > 1 then
+						-- If more than 1, there's no need to keep counting.
+
+						break
+					end
 				end
 
-				-- TODO: Optimize by breaking if > 1?
-				-- TODO: calculate language at the top of the iteration and break if already > 1?
+				byLanguage:increment( languageFromRegion, linesCountForRegion )
 			end
-
-			-- byRegion:increment( region, linesCountForRegion )
-
-			local languageFromRegion = DATA.getLanguage( region.index ) -- TODO: account for Asian-English
-
-			byLanguage:increment( languageFromRegion, linesCountForRegion )
 		end
 	end
 
@@ -73,27 +83,37 @@ end
 
 local function test()
 	local computedCategories = computeCategories( {
-		en_sets = [[
+		eu_sets = [[
 
+			DR3-EN192; Dark Revelation Volume 3; Super Rare
+			DR3-EN192; Dark Revelation Volume 3; Super Rare
 			DR3-EN192; Dark Revelation Volume 3; Super Rare
 
 		]],
 		kr_sets = [[
 			TLM-KR012; The Lost Millennium; Super Rare, Ultimate Rare
-			SD7-KR009; Structure Deck: Invincible Fortress; Common
-			HGP3-KR192; Expert Edition Volume 3; Super Rare
 		]],
 		fr_sets = [[
 			LDD-F001; Legend of Blue Eyes White Dragon; Ultra Rare
 		]],
 		fc_sets = [[
 			LDD-C001; Legend of Blue Eyes White Dragon; Ultra Rare
-		]]
+		]],
+		jp_sets = [[
+			LDD-C001; Legend of Blue Eyes White Dragon; Ultra Rare
+			LDD-C001; Legend of Blue Eyes White Dragon; Ultra Rare
+			LDD-C001; Legend of Blue Eyes White Dragon; Ultra Rare
+		]],
+		ja_sets = [[]],
 	} )
 
-	mw.log( computedCategories )
+	local check = (
+		computedCategories == '[[Category:Korean language releases without a reprint]][[Category:French language releases without a reprint]]'
+	)
 
-	return computedCategories == '[[Category:English language releases without a reprint]]'
+	mw.log( computedCategories, check )
+
+	return check
 end
 
 
