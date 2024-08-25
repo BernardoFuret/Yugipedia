7629e74a99824504c437d4c59b13a4c7e98382f3
BernardoFuret <bernardo.furet@hotmail.com>
2024-08-25 23:48:03 +0100

    gadgets(SetGalleryToC): Display nested sections (WIP)

--- Gadgets/SetGalleryToC/SetGalleryToC.js
+++ Gadgets/SetGalleryToC/SetGalleryToC.js
@@ -9,39 +9,89 @@
 
 	var LAST_LOG = '~~~~~';
 
+	function newToc() {
+		return  $( '<ul>', {
+			'class': 'set-gallery__toc__list'
+		} );
+	}
+
+	function getHeaderLevel( headerTag ) {
+		var match = /H(?<level>\d)/.exec( headerTag );
+
+		return ( match && match.groups && Number( match.groups.level ) ) || null;
+	}
+
 	function generateToC( $content ) {
 		var $tocPlaceholder = $content.find( '.set-gallery__toc' );
 
-		var $contentSectionHeaders = $content.find( '.mw-headline' );
+		var $contentSectionsTitles = $content.find( '.mw-headline' );
 
-		if ( $contentSectionHeaders.length >= 3 ) { // TODO: make configurable per user?
+		if ( $contentSectionsTitles.length >= 3 ) { // TODO: make configurable per user?
 			var $tocContainer = $( '<div>', {
 				'class': 'set-gallery__toc__container'
 			} ).appendTo( $tocPlaceholder );
 
-			var $tocList = $( '<ul>' ).appendTo( $tocContainer );
+			var $initialTocList = newToc().appendTo( $tocContainer )
 
-			$contentSectionHeaders.each( function( index, element ) {
-				var $sectionTitle = $( element );
+			var tocsStack = [ $initialTocList ];
 
-				var sectionTitleId = $sectionTitle.attr( 'id' );
+			var headersStack = []
 
-				var sectionTitleHtml = $sectionTitle.html();
+			$contentSectionsTitles.each( function( index, element ) {
+				var $currentSectionTitle = $( element );
 
-				$tocList.append( function() {
-					var $currentSectionTitle = $( this )
+				var currentSectionTitleId = $currentSectionTitle.attr( 'id' );
+
+				var currentSectionTitleHtml = $currentSectionTitle.html();
+
+				var $currentSectionHeader = $currentSectionTitle.parent( ':header' );
+
+				var currentSectionHeaderTag = $currentSectionHeader.prop( 'tagName' );
+
+				var previousSectionHeaderTag = headersStack[ 0 ];
+
+				if ( previousSectionHeaderTag == null ) {
+					headersStack.unshift( currentSectionHeaderTag );
+				} else {
+					var previousSectionHeaderLevel = previousSectionHeaderTag && getHeaderLevel( previousSectionHeaderTag );
+
+					var currentSectionHeaderLevel = getHeaderLevel( currentSectionHeaderTag );
+
+					if ( previousSectionHeaderLevel != null && currentSectionHeaderLevel !== null ) {
+						if ( currentSectionHeaderLevel > previousSectionHeaderLevel ) {
+							tocsStack.unshift( newToc().appendTo( tocsStack[ 0 ] ) );
+
+							headersStack.unshift( currentSectionHeaderTag );
+						}
+
+						if ( currentSectionHeaderLevel < previousSectionHeaderLevel ) {
+							tocsStack.shift();
+
+							headersStack.shift( currentSectionHeaderTag );
+						}
+					}
+				}
+
+				var $toc = tocsStack[ 0 ];
+
+				$toc.append( function() {
+					var tocSectionTitleId = $( this )
 						.parents( '.set-gallery' )
 						.prevAll( ':header' )
 						.first()
-						.find( '.mw-headline' );
+						.find( '.mw-headline' )
+						.attr( 'id' );
 
-					var currentSectionTitleId = $currentSectionTitle.attr( 'id' );
+					// Get previous element and compare tags:
+					// If greater level, then append classname that adds a ( to the ::before
+					// If lesser level, then prepend classname that adds a ) to the ::before
+					// If same, as it is now
 
 					return $( '<li>', {
 						'class': 'set-gallery__toc__entry',
-						html: $( currentSectionTitleId === sectionTitleId ? '<strong>' : '<a>', {
-							href: '#' + sectionTitleId,
-							html: sectionTitleHtml
+						html: $( tocSectionTitleId === currentSectionTitleId ? '<strong>' : '<a>', {
+							href: '#' + currentSectionTitleId,
+							html: currentSectionTitleHtml
 						} )
 					} );
 				} );
