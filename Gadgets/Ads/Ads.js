82d4c39823375466d29cd88f980e32d6ebe21515
BernardoFuret <bernardo.furet@hotmail.com>
2026-02-11 00:48:20 +0000

    gadget(ads): Set content ads

--- Gadgets/Ads/Ads.js
+++ Gadgets/Ads/Ads.js
@@ -13,11 +13,48 @@
 		document.getElementById('mw-panel').style.height = document.documentElement.scrollHeight + 'px';
 	}
 
-	mw.loader.using( 'mediawiki.util' ).then(function () {
+	mw.loader.using('mediawiki.util').then(function () {
 		if (!mw.util.getParamValue('action')) {
 			updateSideBarHeight();
 		}
 	});
+
+	function setContentAds() {
+		if (!mw.user.isAnon()) {
+			return;
+		}
+
+		document.querySelectorAll('.mw-headline').forEach(function (element, index) {
+			var parentH2 = element.closest('h2 > .mw-headline');
+
+			if (!parentH2) {
+				return;
+			}
+
+			var adPlacementId = 'content-banner-' + index.toString();
+
+			var adPlacement = document.createElement('div');
+
+			adPlacement.id = adPlacementId;
+
+			parentH2.insertAdjacentElement('beforebegin', adPlacement);
+
+			window.nitroAds.createAd(adPlacementId, {
+				sizes: [
+					['728', '90'],
+					['320', '50']
+				],
+				report: {
+					enabled: true,
+					icon: true,
+					wording: 'Report Ad',
+					position: 'top-right'
+				}
+			});
+		});
+	}
+
+	mw.hook('wikipage.content').add(setContentAds);
 
 	console.log('[Gadget] Ads last updated at', LAST_LOG);
 })();
